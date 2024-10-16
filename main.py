ceede7121b845486c71f9b151a2a59c96a4d297c
Munkhdelger <144759404+Mu4ger@users.noreply.github.com>
2024-10-16 17:49:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import tweepy 
+import openai
+import schedule
+import time
+import random
+
+API_KEY = "your_twitter_api_key"
+API_SECRET_KEY = "your_twitter_api_secret_key"
+ACCESS_TOKEN = "your_twitter_access_token"
+ACCESS_TOKEN_SECRET = "your_twitter_access_token_secret"
